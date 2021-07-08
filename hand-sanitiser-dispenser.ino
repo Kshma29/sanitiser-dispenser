5f3d5abad8801a4028f857454c4105790c60c4b9
Kshma Singh <77921246+Kshma29@users.noreply.github.com>
2021-07-09 01:39:57 +0530

    Update hand-sanitiser-dispenser.ino

--- hand-sanitiser-dispenser.ino
+++ hand-sanitiser-dispenser.ino
@@ -1,18 +1,11 @@
 const int servo = 9;     //define Servo Signal Pin
 const int trigPin = 10;  //define Trigger Pin
 const int echoPin = 11;  //define Echo Pin
-
-// defines variables
-
 long duration;
 int distance;
-
 #include <Servo.h>
-
 Servo myservo;    // create servo object to control a servo
-
 int pos = 0;    // variable to store the servo position
-
 void setup() {
   pinMode(trigPin, OUTPUT);    // Sets the trigPin as an Output
   pinMode(echoPin, INPUT);    // Sets the echoPin as an Input
@@ -20,32 +13,20 @@ void setup() {
   myservo.write(0);         // Sets Servo to initially 0 degrees 
  Serial.begin(9600);      // Starts the serial communication
 }
-
 void loop() {
-    //
     digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    
+    delayMicroseconds(2); 
     // Sets the trigPin on HIGH state for 10 micro seconds
-    
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-    
     // Reads the echoPin, returns the sound wave travel time in microseconds
-    
     duration = pulseIn(echoPin, HIGH);
-    
     // Calculating the distance
-   
     distance= duration*0.034/2;
-    
     // Prints the distance on the Serial Monitor
     Serial.print("Distance: ");
     Serial.println(distance);
-    
-    //Servo
-    
     if(distance<10)
     { //Check distance is less than 10cm 
        myservo.write(45); // Sets Servo in stages from 0 to 180 degrees so soap does not pitch out. 
@@ -59,4 +40,4 @@ void loop() {
        myservo.write(00); // Reset the servo to 0 Degrees
        delay(2500);   //Delay the next time someone can get soap
     }                 
-}a
+}
